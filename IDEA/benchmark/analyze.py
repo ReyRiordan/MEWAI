0a3c0b03b98d4fd2ef5275bbadd95e47d846809c
ReyRiordan <reyriordan@gmail.com>
2025-09-05 00:49:03 -0400

    feat: analyze total time spent for every eval (both human and AI)

---
+++ IDEA/benchmark/analyze.py
@@ -0,0 +1,50 @@
+import time
+from datetime import datetime
+import io
+import os
+import base64
+import json
+import tempfile
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from openai import OpenAI
+from anthropic import Anthropic
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv('.venv/.env')
+DB_URI = os.getenv("DB_URI")
+
+
+# Compute time elapsed in minutes from ISOs
+def elapsed_minutes(iso1: str, iso2: str) -> int:
+    t1 = datetime.fromisoformat(iso1)
+    t2 = datetime.fromisoformat(iso2)
+    delta = abs(t2 - t1)
+    return delta.total_seconds() / 60
+
+def compute_time_spent():
+    client = MongoClient(DB_URI)
+    source = client['Benchmark']['AI_Eval.M2_test']
+    docs = list(source.find())
+
+    for doc in docs:
+        total = 0
+        start = None
+        times = list(doc['times'].items())
+        for index, (time, type) in enumerate(times):
+            if type == "start":
+                if index != 0 and times[index-1][1] == "mark": # if they didn't "end" the previous sesh
+                    total += elapsed_minutes(start, times[index-1][0])
+                start = time
+            elif type == "end":
+                total += elapsed_minutes(start, time)
+            elif index == len(times)-1 and type == "mark":
+                total += elapsed_minutes(start, time)
+
+        doc['time_spent'] = total
+        source.replace_one({"_id": doc['_id']}, doc)
+
+
+if __name__ == "__main__":
+    compute_time_spent()
