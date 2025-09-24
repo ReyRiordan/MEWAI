0f785ea06e4e8d37f85749aceeb46f816d922fc5
ReyRiordan <reyriordan@gmail.com>
2025-09-23 20:36:44 -0400

    base eval working lfg

--- IDEA/benchmark/benchmark.py
+++ IDEA/benchmark/benchmark.py
@@ -10,13 +10,20 @@ from evaluate import *
 
 client = MongoClient(DB_URI)
 group_eval_source = client['Benchmark']['Group_Eval.M2_test']
-source = client['Benchmark']['AI_Eval.M2_test']
-AI_EVALS = list(source.find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
+AI_EVALS = list(client['Benchmark']['AI_Eval.M2_test'].find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
+HUMAN_EVALS = list(client['Benchmark']['Human_Eval.M2_test_copy'].find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
+ALL_EVALS = AI_EVALS + HUMAN_EVALS
 
-
-n_correct = 0
-n_total = 0
-for eval in AI_EVALS:
+results = {}
+for eval in ALL_EVALS:
+    if eval['username'] not in results:
+        results[eval['username']] = {
+            'n_correct': 0,
+            'n_total': 0,
+            'incorrect': []
+        }
+    result = results[eval['username']]
+    
     group_eval = group_eval_source.find_one({
         'sim_info.netid': eval['sim_info']['netid'], 
         'sim_info.patient': eval['sim_info']['patient']
@@ -26,14 +33,15 @@ for eval in AI_EVALS:
         for part in eval['evaluation'][section]:
             correct_features = group_eval['evaluation'][section][part]['features']
             for feature, grade in eval['evaluation'][section][part]['features'].items():
-                n_total += 1
+                result['n_total'] += 1
                 if correct_features[feature] not in ["TRUE", "FALSE", "EITHER"]:
-                    print(f"ERROR: {correct_features[feature]} is unexpected correct value.")
+                    print(f"ERROR @ netid {eval['sim_info']['netid']}, patient {eval['sim_info']['patient']}: {correct_features[feature]} is unexpected correct value.")
                 elif correct_features[feature] == "TRUE" and grade == True:
-                    n_correct += 1
+                    result['n_correct'] += 1
                 elif correct_features[feature] == "FALSE" and grade == False:
-                    n_correct += 1
+                    result['n_correct'] += 1
                 elif correct_features[feature] == "EITHER":
-                    n_correct += 1
+                    result['n_correct'] += 1
 
-print(f"Accuracy: {n_correct}/{n_total} -> {n_correct/n_total}")
+for username, result in results.items():
+    print(f"{username}: {result['n_correct']}/{result['n_total']} -> {result['n_correct']/result['n_total']}")
