1bd1531e08f89839c14c0e8cabf902d6181299c2
ReyRiordan <reyriordan@gmail.com>
2025-11-16 15:29:36 -0500

    eval and benchmarking with new flag paradigm

--- IDEA/benchmark/benchmark.py
+++ IDEA/benchmark/benchmark.py
@@ -5,7 +5,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from lookups import *
-from evaluate import *
+# from evaluate import *
 
 
 # with open("./IDEA/benchmark/group_evals.json", 'r') as correct_file:
@@ -13,12 +13,13 @@ from evaluate import *
 
 client = MongoClient(DB_URI)
 group_eval_source = client['Benchmark']['Group_Eval.M2_test']
-AI_EVALS = list(client['Benchmark']['AI_Eval.M2_test'].find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
+AI_EVALS = list(client['Benchmark']['AI_Eval.M2_test_flag'].find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
 HUMAN_EVALS = list(client['Benchmark']['Human_Eval.M2_test_copy'].find({}, {'_id': 0, 'username': 1, 'model_info': 1, 'sim_info': 1, 'evaluation': 1}))
-ALL_EVALS = AI_EVALS + HUMAN_EVALS
+# ALL_EVALS = AI_EVALS + HUMAN_EVALS
 
 results = {}
-for eval in ALL_EVALS:
+
+for eval in HUMAN_EVALS:
     if eval['username'] not in results:
         temp = {
             'all': {'correct': 0, 'total': 0},
@@ -30,8 +31,8 @@ for eval in ALL_EVALS:
                      "Explanation of Alternative Diagnoses", 
                      "Plan"]:
             temp[part] = {'correct': 0, 'total': 0}
-        for feature in ["A", "B", "C"]:
-            temp["Explanation of Lead Diagnosis"][feature] = 0
+        # for feature in ["A", "B", "C"]:
+        #     temp["Explanation of Lead Diagnosis"][feature] = 0
         results[eval['username']] = temp
 
     result = results[eval['username']]
@@ -47,29 +48,90 @@ for eval in ALL_EVALS:
             for feature, grade in eval['evaluation'][section][part]['features'].items():
                 result[part]['total'] += 1
                 result['all']['total'] += 1
-                if correct_features[feature] not in ["TRUE", "FALSE"]:
+                if correct_features[feature] not in [True, False]:
                     print(f"ERROR @ netid {eval['sim_info']['netid']}, patient {eval['sim_info']['patient']}: {correct_features[feature]} is unexpected correct value.")
-                elif correct_features[feature] == "TRUE" and grade == True:
+                elif correct_features[feature] == True and grade == True:
                     result[part]['correct'] += 1
                     result['all']['correct'] += 1
-                    if part == "Explanation of Lead Diagnosis":
-                        result[part][feature] += 1
-                elif correct_features[feature] == "FALSE" and grade == False:
+                    # if part == "Explanation of Lead Diagnosis":
+                    #     result[part][feature] += 1
+                elif correct_features[feature] == False and grade == False:
                     result[part]['correct'] += 1
                     result['all']['correct'] += 1
-                    if part == "Explanation of Lead Diagnosis":
-                        result[part][feature] += 1
+                    # if part == "Explanation of Lead Diagnosis":
+                    #     result[part][feature] += 1
+
+for eval in AI_EVALS:
+    if eval['username'] not in results:
+        temp = {
+            'all': {'correct': 0, 'total': 0},
+            # 'incorrect': []
+        }
+        for part in ["Summary Statement", 
+                     "Differential Diagnosis", 
+                     "Explanation of Lead Diagnosis", 
+                     "Explanation of Alternative Diagnoses", 
+                     "Plan"]:
+            temp[part] = {'correct': 0, 'total': 0}
+        # for conf in ["high", "medium", "low"]:
+        #     temp[conf] = {'correct': 0, 'total': 0}
+        for flag in ["flag", "noflag"]:
+            temp[flag] = {'correct': 0, 'total': 0}
+        results[eval['username']] = temp
+
+    result = results[eval['username']]
+    
+    group_eval = group_eval_source.find_one({
+        'sim_info.netid': eval['sim_info']['netid'], 
+        'sim_info.patient': eval['sim_info']['patient']
+        })
+    
+    for section in group_eval['evaluation']:
+        for part in group_eval['evaluation'][section]:
+            correct_features = group_eval['evaluation'][section][part]['features']
+            for feature, grading in eval['evaluation'][part]['features'].items():
+                if 'grade' not in grading:
+                    print(f"ERROR @ netid {eval['sim_info']['netid']}, patient {eval['sim_info']['patient']}: {grading}")
+                grade = grading['grade']
+                # conf = grading['confidence']
+                if grading['flag'] == True:
+                    flag = "flag"
+                else:
+                    flag = "noflag"
+
+                result[part]['total'] += 1
+                # result[conf]['total'] += 1
+                result[flag]['total'] += 1
+                result['all']['total'] += 1
+                if feature not in correct_features:
+                    print(f"ERROR @ netid {eval['sim_info']['netid']}, patient {eval['sim_info']['patient']}: {feature} is unexpected feature value.")
+                if correct_features[feature] not in [True, False]:
+                    print(f"ERROR @ netid {eval['sim_info']['netid']}, patient {eval['sim_info']['patient']}: {correct_features[feature]} is unexpected correct value.")
+                elif correct_features[feature] == True and grade == True:
+                    result[part]['correct'] += 1
+                    # result[conf]['correct'] += 1
+                    result[flag]['correct'] += 1
+                    result['all']['correct'] += 1
+                elif correct_features[feature] == False and grade == False:
+                    result[part]['correct'] += 1
+                    # result[conf]['correct'] += 1
+                    result[flag]['correct'] += 1
+                    result['all']['correct'] += 1
 
                 
-for part, result in results["Fac1"].items():
-    print(f"{part}:")
+for cat, result in results["Fac1"].items():
+    print(f"{cat}:")
     for username in results:
-        result = results[username][part]
+        result = results[username][cat]
         print(f"{username}: {result['correct']}/{result['total']} -> {result['correct']/result['total']}")
     print("\n")
 
-    if part == "Explanation of Lead Diagnosis":
-        for username in results:
-            result = results[username][part]
-            for feature in ["A", "B", "C"]:
-                print(f"{username} {feature}: {result[feature]}/30 -> {result[feature]/30}")
+# for conf in ["high", "medium", "low"]:
+#     result = results["anthropic/claude-sonnet-4.5"][conf]
+#     print(f"{conf}: {result['correct']}/{result['total']} -> {result['correct']/result['total']}")
+
+for flag in ["flag", "noflag"]:
+    print(f"{flag}:")
+    for username in ["anthropic/claude-sonnet-4.5", "openai/gpt-5", "anthropic/claude-haiku-4.5"]:
+        result = results[username][flag]
+        print(f"{username}: {result['correct']}/{result['total']} -> {result['correct']/result['total']}")
--- IDEA/benchmark/evaluate.py
+++ IDEA/benchmark/evaluate.py
@@ -14,64 +14,113 @@ import re
 import requests
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from lookups import *
+import json
+from dotenv import load_dotenv
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from lookups import *
+
+load_dotenv('.venv/.env')
+DB_URI = os.getenv("DB_URI")
+TARGET_COLLECTION = "AI_Eval.M2_test_flag"
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+PROMPT_ID = "Evalflag_11-14-25"
+BASE_RUBRIC_ID = "base_new"
+RUBRIC_ID = "atypicals_11-14-25"
+
+with open(f"./Rubrics/{BASE_RUBRIC_ID}.json", "r") as rubric_base_file:
+    rubric_base = json.load(rubric_base_file)
+with open(f"./Rubrics/{RUBRIC_ID}.json", "r") as grading_rubric_file:
+    grading_rubric = json.load(grading_rubric_file)
+
+RUBRIC = {}
+for part in rubric_base:
+    if part in ["Key Findings", "HPI1", "HPI2", "HPI3", "PH1", "PH2"]: 
+        continue
+    else:
+        RUBRIC[part] = {**rubric_base[part], **grading_rubric[part]}
+
 
 def create_prompt(prompt: str, rubric: dict) -> str:
-    prompt = prompt.replace("<title></title>", f"<title>{rubric['title']}</title>")
-    prompt = prompt.replace("<desc></desc>", f"<desc>{rubric['desc']}</desc>")
+    prompt = prompt.replace("{{title}}", rubric['title'])
+    prompt = prompt.replace("{{desc}}", rubric['desc'])
 
-    def format_rubric(rubric: dict) -> str:
-        lines = ["FEATURES:"]
-        for key, desc in rubric['features'].items():
-            lines.append(f"\t{key}. {desc}")
+    # def format_rubric(rubric: dict) -> str:
+    #     lines = ["FEATURES:"]
+    #     for key, desc in rubric['features'].items():
+    #         lines.append(f"\t{key}. {desc}")
         
-        lines.append("\nSCORING:")
-        for points, criteria in rubric['points'].items():
-            lines.append(f"\t{points} points: {criteria}")
+    #     lines.append("\nSCORING:")
+    #     for points, criteria in rubric['points'].items():
+    #         lines.append(f"\t{points} points: {criteria}")
         
-        return "\n".join(lines)
+    #     return "\n".join(lines)
     
-    formatted_rubric = format_rubric(rubric)
-    prompt = prompt.replace("<rubric></rubric>", f"<rubric>{formatted_rubric}</rubric>")
+    # formatted_rubric = format_rubric(rubric)
+    extracted_rubric = {
+        "features": rubric['features'],
+        "scoring": rubric['scoring']
+    }
+    prompt = prompt.replace("{{rubric}}", str(extracted_rubric))
 
     return prompt
 
 
-def extract_from_output(output: str) -> dict:
-
-    def extract(tag: str):
-        match = re.search(rf'<{tag}>([\s\S]*?)</{tag}>', output)
-        if match:
-            return match.group(1).strip()
-        else:
-            print(f"ERROR: no match for <{tag}> in output")
-            return None
-
-    rationale = extract("rationale")
-    raw_grades = extract("grades")
-    if raw_grades:
-        try:
-            grade_dict = json.loads(raw_grades)
-            features = grade_dict['features']
-            score = grade_dict['score']
-        except json.JSONDecodeError as e:
-            print(f"ERROR: Could not parse grade JSON '{raw_grades}': {e}")
-            grade_dict = features = score = None
+def extract_from_output(output_raw: str) -> dict:
+    m = re.search(r"```json\s*(.*?)\s*```", output_raw, re.DOTALL)
+    if m:
+        output = m.group(1)
     else:
-        grade_dict = features = score = None
-    # feedback = extract("feedback")
+        output = output_raw.strip()
+    
+    try:
+        output_dict = json.loads(output)
+    except json.JSONDecodeError as e:
+        print(f"ERROR: Could not parse grade JSON")
+        print(output_raw)
 
-    return {
-        'comment': rationale,
-        'features': features,
-        'score': score,
-        # 'feedback': feedback
-    }
+    eval = {"features": {}}
+    for key in output_dict:
+        if key != "score":
+            eval['features'][key] = output_dict[key]
+    eval['score'] = output_dict['score']
+
+    return eval
+
+    # def extract(tag: str):
+    #     match = re.search(rf'<{tag}>([\s\S]*?)</{tag}>', output)
+    #     if match:
+    #         return match.group(1).strip()
+    #     else:
+    #         print(f"ERROR: no match for <{tag}> in output")
+    #         return None
+
+    # rationale = extract("rationale")
+    # raw_grades = extract("grades")
+    # if raw_grades:
+    #     try:
+    #         grade_dict = json.loads(raw_grades)
+    #         features = grade_dict['features']
+    #         score = grade_dict['score']
+    #     except json.JSONDecodeError as e:
+    #         print(f"ERROR: Could not parse grade JSON '{raw_grades}': {e}")
+    #         grade_dict = features = score = None
+    # else:
+    #     grade_dict = features = score = None
+    # # feedback = extract("feedback")
+
+    # return {
+    #     'comment': rationale,
+    #     'features': features,
+    #     'score': score,
+    #     # 'feedback': feedback
+    # }
 
 # https://openrouter.ai/docs/api-reference/chat-completion
 def generate(model_info: dict, base_prompt: str, rubric: dict, user_prompt: str) -> dict:
     system_prompt = create_prompt(base_prompt, rubric)
+    print("System: ", len(system_prompt))
+    print("User: ", len(user_prompt))
 
     url = "https://openrouter.ai/api/v1/chat/completions"
     payload = {
@@ -102,7 +151,7 @@ def generate(model_info: dict, base_prompt: str, rubric: dict, user_prompt: str)
     except:
         print(raw)
         raise ValueError()
-    eval = extract_from_output(output) # reasoning, grade, feedback
+    eval = extract_from_output(output) # features (rationale, grade, confidence), score
     usage = {
         'input_tokens': raw['usage']['prompt_tokens'],
         'output_tokens': raw['usage']['completion_tokens']
@@ -116,7 +165,7 @@ def evaluate(model_id: str, which: str, netid = None, patient = None) -> None:
     # DB SETTINGS
     client = MongoClient(DB_URI)
     source = client['Benchmark']['Interviews.M2_test']
-    target = client['Benchmark']['AI_Eval.M2_test']
+    target = client['Benchmark'][TARGET_COLLECTION]
     
     # GET SIMS TO EVAL
     sims = ""
@@ -128,21 +177,9 @@ def evaluate(model_id: str, which: str, netid = None, patient = None) -> None:
             sims = [sim for sim in sims if not target.find_one({"model_info.id": model_id,
                                                                 "sim_info.netid": sim['netid'],
                                                                 "sim_info.patient": sim['patient']})]
-    
-    # MODEL SETTINGS
-    model_info = {
-        "id": model_id,
-        "temperature": None,
-        "thinking": True,
-        "prompt_id": "Evaluate_10-20-25",
-        "usage": {
-            "input_tokens": 0,
-            "output_tokens": 0
-        }
-    }
-    
+        
     # EVAL PROMPT
-    with open(f"./Prompts/{model_info['prompt_id']}.txt", 'r') as prompt_file:
+    with open(f"./Prompts/{PROMPT_ID}.txt", 'r') as prompt_file:
         base_prompt = prompt_file.read()
 
     n = 0
@@ -155,30 +192,34 @@ def evaluate(model_id: str, which: str, netid = None, patient = None) -> None:
             "patient": sim['patient'], 
         }
 
+        model_info = {
+            "id": model_id,
+            "temperature": None,
+            "thinking": True,
+            "prompt_id": PROMPT_ID,
+            "usage": {
+                "input_tokens": 0,
+                "output_tokens": 0
+            }
+        }
+
         n += 1
         print(f"({n}/{len(sims)}) {sim['netid']} | {sim['patient']}")
 
         evaluation = {}
         post_note = sim['post_note_inputs']
-        for section in RUBRIC:
-            student_response = post_note[section]
-            if not student_response:
-                evaluation[section] = None
-                continue
+        for part, rubric in RUBRIC.items():
+            user_prompt = ""
+            for section in rubric['sections']:
+                user_prompt += f"<{section}>{post_note[section]}</{section}>\n"
             
-            evaluation[section] = {}
-            for part, rubric in RUBRIC[section].items():
-                user_prompt = f"<{section}>{student_response}</{section}>"
-                if "extra_context" in rubric:
-                    user_prompt = f"<{section}>{student_response}</{section}>"
-                    extra = rubric['extra_context']
-                    user_prompt += f"\n<{extra}>{post_note[extra]}</{extra}>"
 
-                part_eval = generate(model_info, base_prompt, rubric, user_prompt)
-                part_usage = part_eval.pop('usage')
-                model_info['usage']['input_tokens'] += part_usage['input_tokens']
-                model_info['usage']['output_tokens'] += part_usage['output_tokens']
-                evaluation[section][part] = part_eval
+            part_eval = generate(model_info, base_prompt, rubric, user_prompt)
+            part_usage = part_eval.pop('usage')
+            model_info['usage']['input_tokens'] += part_usage['input_tokens']
+            model_info['usage']['output_tokens'] += part_usage['output_tokens']
+
+            evaluation[part] = part_eval
 
         end_time = datetime.now()
         delta = abs(start_time - end_time)
@@ -198,7 +239,7 @@ def evaluate(model_id: str, which: str, netid = None, patient = None) -> None:
 
 if __name__ == "__main__":
     evaluate(
-        model_id = "anthropic/claude-sonnet-4.5",
+        model_id = "anthropic/claude-haiku-4.5",
         which = "rem",
         # netid = "mi360",
         # patient = "Jeffrey Smith"
